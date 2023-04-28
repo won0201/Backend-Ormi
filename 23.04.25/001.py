3257ea2489f96b34aea1cc7134cc4c8692aec5b7
won0201 <114564396+won0201@users.noreply.github.com>
2023-04-28 13:21:20 +0900

    Add files via upload

---
+++ 23.04.25/001.py
@@ -0,0 +1,13 @@
+# 스니펫 설정
+# {
+#     "Print to console": {
+#         "prefix": "def",
+#         "body": [
+#             "def $1($2):",
+#             "    return $3"
+#         ],
+#         "description": "function 제작"
+#     }
+# }
+def hello(world):
+    return world
