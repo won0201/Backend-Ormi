d7a1c9b3bc71bae2cd77ad1afcc7aa06529e81d0
won0201 <114564396+won0201@users.noreply.github.com>
2023-07-04 23:25:24 +0900

    Django day 7

--- Django/myapp/blog/urls.py
+++ Django/myapp/blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # path(패턴, 매핑) /blog/
     # path("", views.index), # FBV
     # 글 목록 조회
-    path("", views.List.as_view(), name='list'), # /blog/
+    path("", views.Index.as_view(), name='list'), # /blog/
     # 글 상세 조회
     path("detail/<int:pk>/", views.DetailView.as_view(), name='detail'), # /blog/detail/1
     # 글 작성
@@ -20,4 +20,8 @@ urlpatterns = [
     path("detail/<int:pk>/comment/write/", views.CommentWrite.as_view(), name='cm-write'),
     # 코멘트 삭제
     path("detail/comment/<int:pk>/delete/", views.CommentDelete.as_view(), name='cm-delete'),
+    # 태그 작성
+    path("detail/<int:pk>/hashtag/write/", views.HashTagWrite.as_view(), name='tag-write'),
+    # 태그 삭제
+    path("detail/<int:pk>/hashtag/delete/", views.HashTagDelete.as_view(), name='tag-delete'),
 ]
